efbde54c3c9e0fb0fb30d4816beb977333422f62
Rahul Bokka <rbokka@nevada.unr.edu>
2024-05-04 17:09:31 +0000

    FinalProject

--- imageedit.c
+++ imageedit.c
@@ -6,6 +6,7 @@ Purpose: To be able to take an image and edit it per user's preference such as d
 
 #include <stdio.h>
 
+FILE_NAME "lebron.jpg"
 
 void loadimage(){
 
@@ -15,6 +16,14 @@ void loadimage(){
 int main (){
 
     int choice;
+
+    FILE *image;
+
+    image = fopen(FILE_NAME, "r");
+
+    if(fp == NULL){
+        printf("Can't open file \n");
+    }
 
     printf("What would you like to do?");
     printf("1. Load New Image");
@@ -32,6 +41,6 @@ int main (){
 
 loadimage() {
 
-    
+
 
 }
