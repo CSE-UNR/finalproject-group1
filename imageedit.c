36f7da9578d26aa79c0fa8b0ba7692bc5398e9e5
Rahul Bokka <rbokka@nevada.unr.edu>
2024-05-04 16:30:43 +0000

    FinalProject

--- imageedit.c
+++ imageedit.c
@@ -3,3 +3,35 @@ Date: 04 May 2024
 Purpose: To be able to take an image and edit it per user's preference such as dim, brighten, rotate
 */
 
+
+#include <stdio.h>
+
+
+void loadimage(){
+
+
+}
+
+int main (){
+
+    int choice;
+
+    printf("What would you like to do?");
+    printf("1. Load New Image");
+    printf("2. Display the Current Image");
+    printf("3. Edit the Current Image");
+    printf("4. Exit the Program");
+    scanf("%d", &choice);
+
+
+
+
+
+
+}
+
+loadimage() {
+
+    
+
+}
