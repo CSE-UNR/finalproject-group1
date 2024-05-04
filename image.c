a3faed82ba753ec9ec931da67129bbeeeede2443
Rahul Bokka <rbokka@nevada.unr.edu>
2024-05-04 21:48:08 +0000

    FinalProject

--- image.c
+++ image.c
@@ -10,8 +10,55 @@ Purpose: To be able to take an image and edit it per user's preference such as d
 #define IMAGENAME 100
 #define FILE_NAME "lebron.jpg"
 
+
+int displaymenu();
+
 int main (){
 
+    int choicefinal;
+    int choice;
+
+    choicefinal = displaymenu();
+
+
+
+        if(choicefinal == 1){
+
+            char imagename[IMAGENAME];
+            printf("What is the name of the image file? ");
+            scanf("%s", imagename);
+            
+            FILE *image;
+
+            image = fopen(FILE_NAME, "r");
+
+            if(image == NULL){
+                printf("\nCould not find an image with that filename.");
+            
+            }
+
+        else{
+
+                fclose(image);
+                printf("Loaded Image successfully");
+
+            }
+        }
+
+        else{
+            printf("Invalid option, please try again.\n");
+
+        }
+        
+    
+        return 0;    
+
+
+}
+
+int displaymenu(){
+
+    int choice; 
 
     printf("**ERINSTAGRAM**\n");
     printf("1. Load Image\n");
@@ -21,17 +68,10 @@ int main (){
     printf("\nChoose from one of the options above: ");
     scanf("%d", &choice);
 
-    if(choice == 1){
-
-        char imagename[IMAGENAME];
-        printf("What is the name of the image file? ");
-        scanf("%s", imagename);
-        printf("%s",imagename);
-        
-    }
-
-
+    return choice;
 
 }
 
-
+void loadimage (){
+    
+}
